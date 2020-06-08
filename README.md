3259ed72c520d377b3b8c00f0c73480589fdf8a1
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-08 23:31:25 +0530

    ported from viveknigam3003/rucio-notifier

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # notifier
-Rucio Notification Utility with a graphical user interface (GSOC Project)
+Rucio Notification Utility with a graphical user interface. This app uses React v16.13.1 and Electron v7.0.0
+
+## Getting Started
+Fork the repository or clone it directly to run on your system.
+
+```BASH
+$ git clone https://github.com/<your-username>/rucio-notifier.git
+$ cd rucio-notifier
+```
+
+__Starting the React and Electron Servers (Dev)__
+
+To start the React dev server
+
+```BASH
+$ npm install && npm start
+```
+
+To run the electron app window with React. Open a new terminal window and run
+
+```BASH
+$ npm run electron
+```
+
+This will start the React dev server inside of an Electron window. Any changes will be refleted live in the app.
+
+## Troubleshooting
+
+In case of an `code ELIFECYCLE` npm error, perform the following steps
+
+```BASH
+$ npm cache clean --force
+$ rm -rf node_modules package-lock.json
+$ npm install && npm start
+```
+
+## Notes
+
+This project is under development as part of the Google Summer of Code 2020 project for CERN-HSF's Rucio.
+It is not yet ready for production release.
---
+++ src/main.js
@@ -0,0 +1,128 @@
+const { app, BrowserWindow, ipcMain, Tray, Menu, nativeImage } = require("electron");
+const path = require("path");
+
+let tray = undefined;
+let window = undefined;
+
+const os = require("os");
+const platforms = {
+  WINDOWS: "WINDOWS",
+  MAC: "MAC",
+  LINUX: "LINUX"
+};
+
+const platformsNames = {
+  win32: platforms.WINDOWS,
+  darwin: platforms.MAC,
+  linux: platforms.LINUX
+};
+
+const currentPlatform = platformsNames[os.platform()];
+
+app.on("ready", () => {
+  createTray();
+  createWindow();
+});
+
+const createTray = () => {
+  var iconPath = path.join(__dirname, "rucioSqLogo.png");
+  let trayIcon = nativeImage.createFromPath(iconPath);
+  tray = new Tray(trayIcon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Rucio",
+      click: () => {
+        showWindow();
+      }
+    },
+    {
+      label: "Separator",
+      type: "separator"
+    },
+    {
+      label: "Help Centre",
+      click: () => {
+        showHelp();
+      }
+    },
+    {
+      label: "Quit",
+      click: () => {
+        window.destroy();
+        tray.destroy();
+      }
+    }
+  ]);
+  tray.setToolTip("Rucio");
+  tray.on("click", function(event) {
+    toggleWindow();
+  });
+  tray.setContextMenu(contextMenu);
+};
+
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds();
+  const trayBounds = tray.getBounds();
+
+  if (currentPlatform != "LINUX") {
+    // Center window horizontally below the tray icon
+    const x = Math.round(
+      trayBounds.x - trayBounds.width / 2 + windowBounds.width / 2 - 320
+    );
+
+    // Position window 4 pixels vertically below the tray icon
+    const y = Math.round(trayBounds.y - trayBounds.height - 440);
+
+    return { x: x, y: y };
+  } else {
+    return { x: 1500, y: 5 };
+  }
+};
+
+//-40 in size for setting the constant value in position
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 360,
+    height: 480,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: false,
+    skipTaskbar: true,
+    scrollable: false,
+    backgroundColor: "#fffafa",
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  });
+
+  // Hide the window when it loses focus
+   window.on('blur', () => {
+     if (!window.webContents.isDevToolsOpened()) {
+       window.hide()
+     }
+   })
+};
+
+const toggleWindow = () => {
+  window.isVisible() ? window.hide() : showWindow();
+};
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.loadURL('http://localhost:3001');
+  window.show();
+};
+
+const showHelp = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+  window.loadURL('http://localhost:3001');
+  window.show();
+};
+
+ipcMain.on("show-window", () => {
+  showWindow();
+});
