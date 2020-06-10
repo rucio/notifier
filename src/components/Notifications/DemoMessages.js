44247f87d7befcc59d08f78ade49eab54f94be2b
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-11 03:00:07 +0530

    Fix: Minor Fixes

--- src/components/Notifications/DemoMessages.js
+++ src/components/Notifications/DemoMessages.js
@@ -3,13 +3,13 @@ const messages = [
     id: 0,
     primary: "Welcome to Rucio!",
     secondary: "This is the notifications panel",
-    read: false
+    isread: false
   },
   {
     id: 1,
     primary: "Setup Complete",
     secondary: "You can start using the app",
-    read: false
+    isread: false
   },
 ];
 
