22db8ba973863b07df35b73e625d610679a962b5
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-12 20:10:30 +0530

    Fix: Added Keys in Message Metadata

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
         <Navbar />
       </div>
     );
-
   }
 }
 
--- src/components/Notifications/DemoMessages.js
+++ src/components/Notifications/DemoMessages.js
@@ -1,16 +1,18 @@
-const messages = [
+const demoMessages = () => [
   {
     id: 0,
     primary: "Welcome to Rucio!",
     secondary: "This is the notifications panel",
-    isread: false
+    server: "rucio-dev-server",
+    read: false
   },
   {
     id: 1,
     primary: "Setup Complete",
     secondary: "You can start using the app",
-    isread: false
+    server: "rucio-dev-server",
+    read: false
   },
 ];
 
-export default messages;
+export default demoMessages;
