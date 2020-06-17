f04583f07712d3e9af43f9ea7b777550c3a76321
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-18 03:34:39 +0530

    Enhancement: Refactoring for Readability.

--- server/app.js
+++ server/app.js
@@ -9,8 +9,8 @@ RUCIO_TOKEN = {token: "", expires: ""};
 
 app.use(cors({ origin: "http://localhost:3003" }));
 app.use(express.json());
-app.use(auth);
 app.use(ping);
+app.use(auth);
 
 app.listen(port, () =>
   console.log(`[INFO] Rucio Notifier listening at http://localhost:${port}`)
