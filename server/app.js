ec6ba0a98080ca101e23c91161865f699598a179
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-18 00:01:50 +0530

    Fix: Improved Token Handling

--- server/app.js
+++ server/app.js
@@ -6,10 +6,9 @@ var cors = require("cors");
 var router = express.Router();
 const config = require("../src/components/Options/config.json");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 const port = 3004;
 const app = express();
+
 app.use(cors({ origin: "http://localhost:3003" }));
 app.use(express.json());
 
@@ -17,29 +16,40 @@ router.get("/", (req, res) => {
   res.json({ message: `Rucio Notifier Running on ${port}` });
 });
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const httpsAgent = new https.Agent({ ca: fs.readFileSync(config[0].cacert) });
 
-router.post("/login/userpass", (req, res) => {
+var RUCIO_TOKEN = null;
+
+getTokenWithUserpass = (req, res, next) => {
   axios
     .get(`https://localhost/auth/userpass`, {
       httpsAgent,
       headers: req.body.payload,
     })
     .then((response) => {
-      res.send({
-        token: response.headers['x-rucio-auth-token'],
-        expires: response.headers['x-rucio-auth-token-expires'],
-      });
-      console.log("[INFO] Success");
+      RUCIO_TOKEN = {
+        token: response.headers["x-rucio-auth-token"],
+        expires: response.headers["x-rucio-auth-token-expires"],
+      };
+      console.log("[INFO] Token Received");
+      res.sendStatus(200);
+      next();
     })
     .catch((error) => {
-      console.log("[WARNING] " + error);
-      res.sendStatus(500);
+      console.log(`[DEBUG] ${error}`);
+      res.sendStatus(error.toString().split(" ").pop());
+      next();
     });
+};
+
+router.post("/login/userpass", getTokenWithUserpass, (req, res) => {
+  if (RUCIO_TOKEN === null) console.log("[ERROR] Authentication Failed");
+  else console.log(`[INFO] Token Expires: ${RUCIO_TOKEN.expires}`);
 });
 
 app.use("/rucio", router);
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Rucio Notifier listening at http://localhost:${port}`)
 );
