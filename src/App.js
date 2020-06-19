3e3043dadfbdb23d46c578bd43e7d2ea7a7dbf13
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-19 22:40:06 +0530

    Fix: App Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-import Header from "./Layout/Header";
-import Navbar from "./Layout/Navbar";
+import AppLayout from "./Layout/AppLayout";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="container-small">
-        <Header />
-        <Navbar />
+        <AppLayout/>
       </div>
     );
   }
