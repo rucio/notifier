d058b913b23e6c28b16bbe276ce98cfb8d1eea1c
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-20 01:59:19 +0530

    Changed App to functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import AppLayout from "./Layout/AppLayout";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container-small">
-        <AppLayout/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container-small">
+      <AppLayout />
+    </div>
+  );
 }
 
 export default App;
