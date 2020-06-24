a88dbf2b337effa59cf5cfa3778177a2d8db34e2
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-06-25 01:19:48 +0530

    Fixed #10: Adding Multi Server Support

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,41 @@
 const express = require("express");
+const { getTokenWithUserpass } = require("../middleware/userpass");
+const { parseServers } = require("../utils/parseServers");
 const router = express.Router();
-userpassAuth = require('../middleware/userpass');
 
 /**
  * Checks if the token is still valid at the current time.
  * @param {{token: String, expires: String}} token RUCIO_AUTH_TOKEN object
  */
-function validateToken (token) {
+function validateToken(token) {
   return Date.now() < Date.parse(token.expires);
 }
 
-router.post("/login/userpass", userpassAuth.getToken, (req, res) => {
+router.post("/login/userpass", async (req, res) => {
+  const servers = req.body.servers;
+  const serverURLs = parseServers(servers);
+  authCount = 0;
+  for (i = 0; i < serverURLs.length; i++) {
+    try {
+      await getTokenWithUserpass(
+        req,
+        res,
+        serverURLs[i].url,
+        serverURLs[i].name
+      );
+    } catch (e) {
+      console.log(e);
+    }
+    authCount++;
+    if (authCount === serverURLs.length) res.sendStatus(200);
+  }
+
   if (res.statusCode != 200) {
     console.log("[ERROR] Authentication Failed");
-    RUCIO_TOKEN = {token: '', expires: ''}
+    RUCIO_TOKEN = { token: "", expires: "" };
+  } else {
+    console.log(`[INFO] Authenticated at Connected Servers!`);
   }
-  else {
-    console.log(`[INFO] Token Expires: ${RUCIO_TOKEN.expires}`);}
 });
 
 module.exports = router;
