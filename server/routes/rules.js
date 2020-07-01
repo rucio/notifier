ca5bc996f7e357863f22580c9dea2926b45ab84e
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-01 16:44:03 +0530

    Feature: Adding rules routes

---
+++ server/routes/rules.js
@@ -0,0 +1,26 @@
+const router = require("./auth");
+const { listRules } = require("../APIs/rules");
+
+router.post("/getallrules", async (req, res) => {
+  const accounts = req.body.accounts;
+  const certlocation = req.body.certlocation;
+  const accountRules = [];
+
+  for (i = 0; i < accounts.length; i++) {
+    await listRules(
+      certlocation,
+      accounts[i].server.hostURL,
+      accounts[i].token.token
+    )
+      .then((response) => {
+        accountRules.push({
+          [accounts[i].server.name]: response.data.trim().split("\n"),
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  res.json(accountRules);
+});
+
+module.exports = router;
