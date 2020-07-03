066fc0723a98f3bd41628e96d4c8722d23b9606c
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-03 21:41:27 +0530

    Added Rule ID API

--- server/APIs/rules.js
+++ server/APIs/rules.js
@@ -14,7 +14,24 @@ async function listRules(certlocation, hostAddress, token) {
   return axios.get(`https://${hostAddress}/rules/`, {
     httpsAgent,
     headers: { "X-Rucio-Auth-Token": token },
-  });
+  }).then(console.log(`[INFO] All rules retrieved from ${hostAddress}`));
+}
+
+/**
+ * Gets the info for a particular rule using the Rule ID.
+ * @param {String} certlocation Location of User Cert for Authentication at the server.
+ * @param {String} hostAddress Server's hostname from where the rules need to be retrieved.
+ * @param {String} token Rucio Auth Token.
+ * @param {String} ruleID Rule ID to get the info for the rule.
+ */
+async function ruleInfo(certlocation, hostAddress, token, ruleID) {
+  const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
+
+  return axios.get(`https://${hostAddress}/rules/${ruleID}`, {
+    httpsAgent,
+    headers: { "X-Rucio-Auth-Token": token },
+  }).then(console.log(`[INFO] Retrieved rule: ${ruleID}`));
 }
 
 exports.listRules = listRules;
+exports.ruleInfo = ruleInfo;
