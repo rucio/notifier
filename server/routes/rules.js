20fb997d14b1a51e09e3a74f2ecad41c52a207d0
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-07-03 21:35:51 +0530

    Improved Response type for Rules

--- server/routes/rules.js
+++ server/routes/rules.js
@@ -4,7 +4,7 @@ const { listRules } = require("../APIs/rules");
 router.post("/getallrules", async (req, res) => {
   const accounts = req.body.accounts;
   const certlocation = req.body.certlocation;
-  const accountRules = [];
+  const accountRules = {};
 
   for (let i = 0; i < accounts.length; i++) {
     await listRules(
@@ -13,13 +13,11 @@ router.post("/getallrules", async (req, res) => {
       accounts[i].token.token
     )
       .then((response) => {
-        accountRules.push({
-          [accounts[i].server.name]: response.data.trim().split("\n"),
-        });
+        accountRules[accounts[i].server.name] = response.data.trim().split("\n");
       })
       .catch((err) => console.log(err));
   }
-
+  
   res.json(accountRules);
 });
 
