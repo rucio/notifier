5c4c8a82c0decddb0214fea8d26d7c1d83a99412
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-08-10 15:51:42 +0530

    Added User-Agent for APIs

--- server/APIs/rules.js
+++ server/APIs/rules.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const https = require("https");
 const fs = require("fs");
+const packageJSON = require("../../package.json");
 
 /**
  * Returns an object with all the rules of a user from a particular server.
@@ -11,10 +12,15 @@ const fs = require("fs");
 async function listRules(certlocation, hostAddress, token) {
   const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
 
-  return axios.get(`https://${hostAddress}/rules/`, {
-    httpsAgent,
-    headers: { "X-Rucio-Auth-Token": token },
-  }).then(console.log(`[INFO] All rules retrieved from ${hostAddress}`));
+  return axios
+    .get(`https://${hostAddress}/rules/`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-notifier/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(console.log(`[INFO] All rules retrieved from ${hostAddress}`));
 }
 
 /**
@@ -27,10 +33,15 @@ async function listRules(certlocation, hostAddress, token) {
 async function ruleInfo(certlocation, hostAddress, token, ruleID) {
   const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
 
-  return axios.get(`https://${hostAddress}/rules/${ruleID}`, {
-    httpsAgent,
-    headers: { "X-Rucio-Auth-Token": token },
-  }).then(console.log(`[INFO] Retrieved rule: ${ruleID}`));
+  return axios
+    .get(`https://${hostAddress}/rules/${ruleID}`, {
+      httpsAgent,
+      headers: {
+        "User-Agent": `rucio-notifier/${packageJSON.version}`,
+        "X-Rucio-Auth-Token": token,
+      },
+    })
+    .then(console.log(`[INFO] Retrieved rule: ${ruleID}`));
 }
 
 exports.listRules = listRules;
--- server/APIs/userpass.js
+++ server/APIs/userpass.js
@@ -2,6 +2,7 @@
 const axios = require("axios");
 const https = require("https");
 const fs = require("fs");
+const packageJSON = require("../../package.json");
 
 /**
  * Attempts to get the RUCIO_AUTH_TOKEN with USERPASS Auth Strategy
@@ -9,15 +10,24 @@ const fs = require("fs");
  * @param {Response} res
  * @param {String} server Server IP to get the token.
  */
-async function getTokenWithUserpass(req, res, serverURL, serverName, account, currentUser, certlocation) {
+async function getTokenWithUserpass(
+  req,
+  res,
+  serverURL,
+  serverName,
+  account,
+  currentUser,
+  certlocation
+) {
   const httpsAgent = new https.Agent({ ca: fs.readFileSync(certlocation) });
   return axios
     .get(`https://${serverURL}/auth/userpass`, {
       httpsAgent,
       headers: {
+        "User-Agent": `rucio-notifier/${packageJSON.version}`,
         "X-Rucio-Account": account.account,
         "X-Rucio-Username": account.username,
-        "X-Rucio-Password": account.password
+        "X-Rucio-Password": account.password,
       },
     })
     .then((response) => {
@@ -28,14 +38,14 @@ async function getTokenWithUserpass(req, res, serverURL, serverName, account, cu
       console.log(`[INFO] Token Received for ${serverName}`);
       res.cookie(`${serverName}`, RUCIO_TOKEN.token, {
         maxAge: 60 * 60 * 1000, // 1 hour
-      })
+      });
       AUTH_COUNT++;
       if (account.account === currentUser.account) CREDS_VALID = true;
     })
     .catch((error) => {
       console.log(`[DEBUG] ${error}`);
       if (account.account === currentUser.account) CREDS_VALID = false;
-      NOT_AUTH++
+      NOT_AUTH++;
     });
 }
 
